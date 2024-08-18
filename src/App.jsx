923a757dcc44b0ed6b9b9068a7af16a01c39ae57
Paolo <macbookpro@Paolos-MBP.station>
2024-08-18 08:43:33 +0200

    start styling App.jsx' git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import styled from "styled-components";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import GlobalStyle from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
+import styled from "styled-components";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -19,11 +14,31 @@ function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-        <Input type="number" placeholder="Number of guests"></Input>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
